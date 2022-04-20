7199dcab06cb9790cc578dc34fbe1722446ed1dd
Juan Jose Aguilarte <barelrincon@gmail.com>
2022-04-20 10:40:38 +0200

    corregido validaciones

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -28,11 +28,12 @@ export class FormularioComponent implements OnInit {
         Validators.required
       ]),
       image: new FormControl('', [
-        Validators.required
+        Validators.required,
+        Validators.pattern(/^[\https://www\.]/)
       ]),
       fecha: new FormControl('', [
         Validators.required,
-        Validators.pattern(/\d{2}-\d{2}-\d{4}/)
+        Validators.pattern(/^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/)
       ]),
       category: new FormControl('', [
         Validators.required
